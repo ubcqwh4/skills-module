5be409d3a714ac483451870fcea4c46fd9dd3dc1
Connie Huang <ucbqwh4@ucl.ac.uk>
2019-11-23 14:07:07 +0000

    before adding relativePositionToStem

---
+++ 01/01_finalProject/shaders/fragment.js
@@ -0,0 +1,44 @@
+// module.exports is the preserved word for exporting
+// copy & paste the frag shader from javascript file
+
+module.exports = `
+precision mediump float;
+
+varying vec2 vUV;
+varying vec3 vNormal;
+
+// uniform vec3 uColor;
+
+float diffuse(vec3 N, vec3 L) {
+	float d = dot(normalize(N), normalize(L));
+	return max(d, 0.0);
+}
+
+#define LIGHT vec3(1.0, 1.0, 1.0)
+
+void main() {
+
+	// get diffused lighting
+	float d = diffuse(vNormal, LIGHT); // 0 ~ 1
+	d = d * 0.5 + 0.5; // 0.5 ~ 1
+
+
+	vec3 uColor = vec3(161.0/255.0, 136.0/255.0, 166.0/255.0);
+	vec3 colorLighting = uColor * d;
+
+	gl_FragColor = vec4(colorLighting, 1.0);
+	// gl_FragColor = vec4(vUV, 0.0, 0.8);
+}`
+
+
+// uniform vec3 uTranslate;
+
+// void main() {
+//   // the position of our square goes from -10 ~ 10
+//   // now map it to 0 ~ 1
+//   float r = map(uTranslate.x, -10.0, 10.0, 0.0, 1.0);
+//   float g = map(uTranslate.y, -10.0, 10.0, 0.0, 1.0);
+//   float b = map(uTranslate.z, -10.0, 10.0, 0.0, 1.0);
+
+//   gl_FragColor = vec4(r, g, b, 1.0);
+// }`
---
+++ 01/01_finalProject/shaders/vertex.js
@@ -0,0 +1,40 @@
+// module.exports is the preserved word for exporting
+// copy & paste the vertex shader from javascript file
+
+module.exports = `
+precision mediump float;
+attribute vec3 aPosition;
+attribute vec2 aUV;
+
+// setup the uniforms for projection / view matrix
+uniform mat4 uProjectionMatrix;
+uniform mat4 uViewMatrix;
+
+// setup the uniform for time
+uniform float uTime;
+uniform float uSize;
+// setup the uniform for translate
+uniform vec3 uTranslate;
+uniform vec3 uTranslateToStem;
+
+// setup varying to pass the uv to the fragment
+varying vec2 vUV;
+varying vec3 vNormal;
+
+void main() {
+  vec3 pos = aPosition;
+
+  // apply size to sphere
+  pos = pos * uSize * 0.01;
+
+  // add the translate to the position
+  pos += uTranslate*0.01;
+
+  // pos += uTranslateToStem * 0.1;
+
+  gl_Position = uProjectionMatrix * uViewMatrix * vec4(pos, 1.0);
+
+  vUV = aUV;
+
+  vNormal = normalize(aPosition);
+}`
